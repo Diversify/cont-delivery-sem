c4b3596e57fabaf5fd56f5d4f79625849ce3d1d4
Kristoffer Richardsson <kristoffer@richardsson.org>
2013-05-11 23:23:56 +0200

    Corrected paths in deploy script

--- build/deploy.sh
+++ build/deploy.sh
@@ -5,11 +5,11 @@ NODE=$1
 echo "Starting deploy to $NODE"
 
 
-./stop_server.sh $NODE
+build/stop_server.sh $NODE
 
 echo "Copying artifacts"
 SRC=webroti/target/webroti-*.war
 DST=/opt/jetty9/webapps/
 scp $SRC $NODE:$DST
 
-./start_server.sh $NODE
+build/start_server.sh $NODE
